b6297fa33e4ad4c595029fc171484ac5abb9657f
Joe-ka <ykaram599@gmail.com>
2023-03-21 16:11:40 +0200

    Update

---
+++ js/master.js
@@ -0,0 +1,11 @@
+const letters = "abcdefghijklmnopqrstuvwxyz";
+let lettersArray = Array.from(letters);
+let lettersContainer = document.querySelector(".letters");
+
+lettersArray.forEach(letter => {
+    let span = document.createElement("span");
+    let theLetter = document.createTextNode(letter);
+    span.appendChild(theLetter);
+    span.className = "letter-box";
+    lettersContainer.appendChild(span);
+});
