6ce9f715b8f6629e639db19a06117998e07de92e
Joe-ka <ykaram599@gmail.com>
2023-03-22 14:32:12 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -9,3 +9,22 @@ lettersArray.forEach(letter => {
     span.className = "letter-box";
     lettersContainer.appendChild(span);
 });
+
+// Object Of Words + Category
+const words = {
+    programming: ["php", "javascript", "go", "scala", "fortran", "r", "mysql", "python"],
+    movies: ["Prestige", "Inception", "Parasite", "Interstellar", "Whiplash", "Memento", "Coco", "Up"],
+    people: ["Albert Einstein", "Hitchock", "Alexander", "Cleopatra", "Mahatma Ghandi"],
+    countries: ["Syria", "Palestine", "Yemen", "Egypt", "Bahrain", "Qatar"],
+};
+
+// Get Random Property
+let allKeys = Object.keys(words);
+let randomPropNumber = Math.floor(Math.random() * allKeys.length);
+let randomPropName = allKeys[randomPropNumber];
+let randomPropValue = words[randomPropName];
+let randomValueNumber = Math.floor(Math.random() * randomPropValue.length);
+let randomValueValue = randomPropValue[randomValueNumber];
+
+// Set Category Info
+document.querySelector(".game-info .category span").innerHTML = randomPropName;
