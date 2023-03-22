a9454e82c618fb6ae8621cd310aab0d4fb1ba4d3
Joe-ka <ykaram599@gmail.com>
2023-03-22 18:10:33 +0200

    Update

--- js/master.js
+++ js/master.js
@@ -28,3 +28,88 @@ let randomValueValue = randomPropValue[randomValueNumber];
 
 // Set Category Info
 document.querySelector(".game-info .category span").innerHTML = randomPropName;
+
+// Select Letters  Guess Element
+let lettersGuessContainer = document.querySelector(".letters-guess");
+
+// Convert Chosen Word To Array
+let lettersAndSpace = Array.from(randomValueValue);
+
+// Create Spans Depened On Word
+lettersAndSpace.forEach(letter => {
+    // Create Empty Span
+    let emptySpan = document.createElement("span");
+    if (letter === " ") {
+        // Add Class To The Span
+        emptySpan.className = "has-space";
+    };
+    // Append Span To The Letters Guess Container
+    lettersGuessContainer.appendChild(emptySpan);
+});
+
+// Select Guess Spans
+let guessSpans = document.querySelectorAll(".letters-guess span");
+
+// Set Wrong Attemps
+let wrongAttemps = 0;
+
+// Select the Draw Element
+let theDraw = document.querySelector(".hangman-draw");
+
+// Handle Clicking On Letters
+document.addEventListener("click", (e) => {
+    // Set The Chose Status
+    let theStatus = false;
+    if (e.target.className === "letter-box") {
+        e.target.classList.add("clicked");
+        // Get Clicked Letter
+        let theClickedLetter = e.target.innerHTML.toLowerCase();
+        // The Chosen Word
+        let theChosenWord = Array.from(randomValueValue.toLowerCase());
+        theChosenWord.forEach((wordLetter, Wordindex) => {
+            // If The Clicked Letter Equal To One Of The Chosen Word Letter 
+            if (theClickedLetter == wordLetter) {
+                // Set Status To Correct
+                theStatus = true;
+                // Loop On All Guess Spans
+                guessSpans.forEach((span, spanindex) => {
+                    if (Wordindex === spanindex) {
+                        span.innerHTML = theClickedLetter
+                    }
+                });
+            };
+        });
+        // If Letter Is Wrong
+        if (theStatus !== true) {
+            // Increase The Wrong Attempts
+            wrongAttemps++;
+            // Add Class Wrong  On The Draw Element
+            theDraw.classList.add(`wrong-${wrongAttemps}`);
+            // Play Fail Sound
+            document.getElementById("fail").play();
+            if (wrongAttemps === 8) {
+                endGame();
+                lettersContainer.classList.add("finished");
+            }
+        } else {
+            // Play success Sound
+            document.getElementById("success").play();
+        };
+    };
+});
+
+// End Game Function
+function endGame() {
+    // Create Popup Div
+    let div = document.createElement("div");
+    // Create text
+    let divText = document.createTextNode(`Game Over, The Word Is "${randomValueValue}"`);
+    // Append Text to Div
+    div.appendChild(divText);
+    // Add Class On Div
+    div.className = "popup";
+    // Append To The Body
+    document.body.appendChild(div);
+    // Play Game Over Sound
+    document.getElementById("game-over").play();
+};
